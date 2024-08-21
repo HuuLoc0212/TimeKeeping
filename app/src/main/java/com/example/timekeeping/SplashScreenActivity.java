54b6931d0983ca10b45d35c27f4eaeda3b0733de
HuuLoc0212 <nguynlc885@gmail.com>
2024-08-21 21:31:09 +0700

    feat(SplashScreen) Add SplashScreen & Logo App

---
+++ app/src/main/java/com/example/timekeeping/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.timekeeping;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },500);
+
+    }
+}
